e7ef2cb39dad9f6a60017e4a4f048d2d1d9c08b7
Jack <guoxinyu0617@gmail.com>
2022-10-15 11:50:31 +0800

    Initial commit

---
+++ src/main/java/com/jack/yupaobackend/YupaoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jack.yupaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YupaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YupaoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jack/yupaobackend/YupaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.yupaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YupaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
